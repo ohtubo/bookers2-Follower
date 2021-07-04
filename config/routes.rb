e4742f6414781512caa175a30c96637161823b0d
ohtubo <brte14020@gmail.com>
2021-07-04 13:51:36 +0000

    [Add]_follower

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,12 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
+
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  # 例えば get '/users/:id/followings' => 'users#followings', as: 'followings' として、usersコントローラで実装することもできます。
+
 end
