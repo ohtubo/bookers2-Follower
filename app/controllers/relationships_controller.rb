29623c65f5af448b0b02adc06a4df03bea71a6e0
ohtubo <brte14020@gmail.com>
2021-07-13 14:44:11 +0000

    [Add]_ajax_1

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,15 +1,21 @@
 class RelationshipsController < ApplicationController
   before_action :authenticate_user!
+
   def create
+    @user = User.find(params[:user_id])#非同期に変数がないため付け加え
     current_user.follow(params[:user_id])
-    redirect_to request.referer
+    #なぜuser_idなのかcurrent_userはどのように渡している？
+    #byebug
+    # redirect_to request.referer
   end
+
   def destroy
+    @user = User.find(params[:user_id])#非同期に変数がないため付け加え
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer
+    # redirect_to request.referer
   end
   # followings, followersは省略
-  
+
   def followings
      user = User.find(params[:user_id])
      @users = user.followings
